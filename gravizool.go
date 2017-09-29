b7dbf57592ebfeb297c5ca927156706a7e4f0f5f
Jeppe Frandsen <jeppe@algrens.dk>
2017-09-29 20:02:41 +0200

    Added fix option for fixing missing semicolons

--- gravizool.go
+++ gravizool.go
@@ -1,64 +1,70 @@
 package main
 
 import (
-    "os"
-    "fmt"
-    "flag"
-    "io/ioutil"
-    "strings"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
 )
 
-const gravizoBegin string = "(http://g.gravizo.com/svg?"
-const gravizoEnd string = "enduml)"
+const gravizoBegin string = "http://g.gravizo.com/svg?"
+const gravizoEnd string = "enduml"
 
-var encoder = strings.NewReplacer(";", "%3B", " ", "%20", "\n", "%0A", "@", "%40",
-    "(", "%28", ")", "%29", "*", "%2A", "\\", "%5C")
-var decoder = strings.NewReplacer("%3B", ";", "%20", " ", "%0A", "\n", "%40", "@",
-    "%2A", "*", "%5C", "\\")
+var gravizoEncode = strings.NewReplacer(";", "%3B", " ", "%20", "\n", "%0A", "@", "%40",
+	"(", "%28", ")", "%29", "*", "%2A", "\\", "%5C")
+var gravizoDecode = strings.NewReplacer("%3B", ";", "%20", " ", "%0A", "\n", "%40", "@",
+	"%2A", "*", "%5C", "\\")
+var removeSemicolons = strings.NewReplacer(";", "")
+var addSemicolons = strings.NewReplacer("\n", ";\n")
 
 func check(err error) {
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
 
 func convert(filename string, replacer *strings.Replacer, backup bool) {
-    buffer, err := ioutil.ReadFile(filename)
-    check(err)
+	buffer, err := ioutil.ReadFile(filename)
+	check(err)
 
-    if backup {
-        err = ioutil.WriteFile(fmt.Sprint(filename +  ".bak"), buffer, 0644)
-        check(err)
-    }
+	if backup {
+		err = ioutil.WriteFile(fmt.Sprint(filename+".bak"), buffer, 0644)
+		check(err)
+	}
 
-    text := string(buffer)
+	text := string(buffer)
 
-    for _, slice := range strings.Split(text, gravizoBegin) {
-        if strings.Contains(slice, gravizoEnd) {
-            subSlice := strings.Split(slice, gravizoEnd)
-            if len(subSlice) > 0 {
-                gravizoText := subSlice[0]
-                convertedText := replacer.Replace(gravizoText)
-                text = strings.Replace(text, gravizoText, convertedText, -1)
-            }
-        }
-    }
+	for _, slice := range strings.Split(text, gravizoBegin) {
+		if strings.Contains(slice, gravizoEnd) {
+			subSlice := strings.Split(slice, gravizoEnd)
+			if len(subSlice) > 0 {
+				gravizoText := subSlice[0]
+				convertedText := replacer.Replace(gravizoText)
+				text = strings.Replace(text, gravizoText, convertedText, -1)
+			}
+		}
+	}
 
-    err = ioutil.WriteFile(filename, []byte(text), 0644)
-    check(err)
+	err = ioutil.WriteFile(filename, []byte(text), 0644)
+	check(err)
 }
 
 func main() {
-    encode := flag.String("e", "", "Encode the given GitHub Markdown file")
-    decode := flag.String("d", "", "Decode the given GitHub Markdown file")
-    backup := flag.Bool("b", true, "Backup GitHub Markdown file before encode/decode")
+	encode := flag.String("e", "", "Encode the GitHub Markdown file")
+	decode := flag.String("d", "", "Decode the GitHub Markdown file")
+	fix := flag.String("f", "", "Fix the GitHub Markdown file")
+	backup := flag.Bool("b", true, "Backup GitHub Markdown file before encode/decode")
 
-    flag.Parse()
+	flag.Parse()
 
-    if len(*encode) > 0 {
-        convert(*encode, encoder, *backup)
-    } else if len(*decode) > 0 {
-        convert(*decode, decoder, *backup)
-    }
+	if len(*encode) > 0 {
+		convert(*encode, gravizoEncode, *backup)
+	} else if len(*decode) > 0 {
+		convert(*decode, gravizoDecode, *backup)
+	} else if len(*fix) > 0 {
+		convert(*fix, removeSemicolons, *backup)
+		convert(*fix, addSemicolons, *backup)
+	}
 }
